4db28dd4604862544f396b387cb2c8d5ecdc4726
iiai22 <114829127+iiai22@users.noreply.github.com>
2023-07-27 17:07:57 -0300

    2 files modified, 1 added

---
+++ fontSelector.js
@@ -0,0 +1,7 @@
+/*function updateh1family() {
+  const selector = document.getElementById('selecth1FontFamily');
+  const family = selector.options[selector.selectedIndex].value;
+  const text = document.getElementById('sink');
+  text.style.fontFamily = family;
+}
+*/
